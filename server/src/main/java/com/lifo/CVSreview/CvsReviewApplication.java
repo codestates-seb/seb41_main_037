4bdc8bd864a6fb46e9a4e7b72e2e53c30918f976
jnjongjeon <jeonij23@gmail.com>
2023-01-05 14:48:45 +0900

    feat: 작업 환경 세팅

---
+++ server/src/main/java/com/lifo/CVSreview/CvsReviewApplication.java
@@ -0,0 +1,13 @@
+package com.lifo.CVSreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvsReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvsReviewApplication.class, args);
+	}
+
+}
