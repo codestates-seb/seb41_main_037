8a0dbb3a994316af56d51e032492da547cc4be57
gkswldus <gkswldus1108@naver.com>
2023-01-13 14:27:51 +0900

    fix: Nav bar 로그인, 회원가입 버튼 위치 수정

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,48 +1,50 @@
-import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const NavBar = styled.main`
-  background-color: #ffcb5e;
+  position: fixed;
+  text-align: center;
   width: 100px;
   height: 100%;
-  padding-top: 10px;
-  text-align: center;
-  position: fixed;
+  background-color: #ffcb5e;
 
   .homeBtn > button {
+    margin-top: 10px;
+    margin-bottom: 70px;
     background-color: transparent;
     border: none;
-    margin-bottom: 70px;
+
     &:hover {
       transition: 1s;
       transform: scale(1.05);
     }
-  }
-
-  .logoBtn {
-    width: 80px;
+    .logoBtn {
+      width: 80px;
+    }
   }
 
   .cvsBtn > button {
     text-align: center;
-    background-color: transparent;
-    border: none;
     width: 100px;
     height: 100px;
+    background-color: transparent;
+    background-position: center;
+    background-size: 60px;
+    background-repeat: no-repeat;
+    color: transparent;
+    border: none;
+
+    &:focus {
+      width: 100px;
+      background-position: center;
+      background-size: 60px;
+      background-color: white;
+      background-repeat: no-repeat;
+    }
   }
 
   .cuBtn {
-    color: transparent;
-    width: 100px;
     background-image: url("img/cu logo_white.png");
-    background-repeat: no-repeat;
-    background-size: 60px;
-    background-position: center;
-    &:focus {
-      width: 100px;
-      background-color: white;
-    }
 
     img {
       width: 60px;
@@ -52,65 +54,37 @@ const NavBar = styled.main`
   }
 
   .gs25Btn {
-    width: 100px;
     background-image: url("img/gs25 logo_white.png");
-    background-repeat: no-repeat;
-    background-size: 60px;
-    background-position: center;
+
     &:focus {
-      width: 100px;
-      background-color: white;
       background-image: url("img/gs25 logo.png");
-      background-repeat: no-repeat;
-      background-size: 60px;
-      background-position: center;
     }
   }
 
   .sevenelevenBtn {
-    width: 100px;
     background-image: url("img/seveneleven logo_white.png");
-    background-repeat: no-repeat;
-    background-size: 60px;
-    background-position: center;
+
     &:focus {
-      width: 100px;
-      background-color: white;
       background-image: url("img/seveneleven logo.png");
-      background-repeat: no-repeat;
-      background-size: 60px;
-      background-position: center;
     }
   }
 
   .userBtn {
-    text-align: center;
+    position: absolute;
+    bottom: 0;
+    flex-direction: column;
     margin-top: 120px;
-    .loginBtn {
-      background-color: #58419c;
-      border: none;
-      border-radius: 50px;
-      color: white;
-      width: 50px;
-      height: 50px;
-      margin: 3px;
-      &:hover {
-        background-color: white;
-        color: #58419c;
-      }
-      &:focus {
-        border: solid 2px #58419c;
-      }
-    }
+    margin-bottom: 15px;
 
-    .signupBtn {
-      background-color: #58419c;
-      border: none;
-      border-radius: 50px;
-      color: white;
+    > button {
       width: 50px;
       height: 50px;
       margin: 3px;
+      background-color: #58419c;
+      color: white;
+      border: none;
+      border-radius: 50px;
+
       &:hover {
         background-color: white;
         color: #58419c;
@@ -124,41 +98,39 @@ const NavBar = styled.main`
 
 const Nav = () => {
   return (
-    <nav>
-      <NavBar>
-        <section className="homeBtn">
-          <button>
-            <Link to="/">
-              <img className="logoBtn" src="img/cvs logo2.png"></img>
-            </Link>
-          </button>
-        </section>
-        <section className="cvsBtn">
-          <button className="cuBtn">
-            <Link to="/itemlist">
-              <img src="img/cu logo.png"></img>
-            </Link>
-          </button>
-          <button className="gs25Btn">
-            <Link to="/itemlist"></Link>
-          </button>
-          <button className="sevenelevenBtn">
-            <Link to="/itemlist"></Link>
-          </button>
-        </section>
-        <section className="userBtn">
-          <button className="loginBtn">
-            <Link to="/login">Login</Link>
-          </button>
-          <button className="signupBtn">
-            <Link to="/signup">
-              Sign <br />
-              up
-            </Link>
-          </button>
-        </section>
-      </NavBar>
-    </nav>
+    <NavBar>
+      <section className="homeBtn">
+        <button>
+          <Link to="/">
+            <img className="logoBtn" src="img/cvs logo2.png"></img>
+          </Link>
+        </button>
+      </section>
+      <section className="cvsBtn">
+        <button className="cuBtn">
+          <Link to="/itemlist">
+            <img src="img/cu logo.png"></img>
+          </Link>
+        </button>
+        <button className="gs25Btn">
+          <Link to="/itemlist"></Link>
+        </button>
+        <button className="sevenelevenBtn">
+          <Link to="/itemlist"></Link>
+        </button>
+      </section>
+      <section className="userBtn">
+        <button className="loginBtn">
+          <Link to="/login">Login</Link>
+        </button>
+        <button className="signupBtn">
+          <Link to="/signup">
+            Sign <br />
+            up
+          </Link>
+        </button>
+      </section>
+    </NavBar>
   );
 };
 
