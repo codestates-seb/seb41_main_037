9236399a0041fa8007c4e27be606aade25b67896
jjangyeunii <euneun0927@naver.com>
2023-01-17 09:23:51 +0900

    feat: adminheader image click event

--- client/src/components/AdminHeader/AdminHeader.tsx
+++ client/src/components/AdminHeader/AdminHeader.tsx
@@ -1,4 +1,5 @@
 // import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const HomePageHeader = styled.header`
@@ -10,14 +11,25 @@ const HomePageHeader = styled.header`
   width: 100vw;
   background-color: #b1b1b1;
   box-shadow: 1px 1px 2px #7a7979;
+  img {
+    &:hover {
+      transform: scale(1.1);
+      transition: 0.5s;
+    }
+  }
 `;
 
 const HomeHeader = () => {
+  const navigate = useNavigate();
+  const handleClickHeader = () => {
+    navigate("/");
+  };
   return (
     <HomePageHeader>
       <img
         src="https://cdn.discordapp.com/attachments/1059647896942809148/1062250836220203069/Logo-removebg-preview.png"
         alt="logo"
+        onClick={handleClickHeader}
       />
     </HomePageHeader>
   );
