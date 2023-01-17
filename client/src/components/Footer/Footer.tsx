44e6e97e2c4b2ecc5a940f837fbab285095a5f9a
gkswldus <gkswldus1108@naver.com>
2023-01-17 12:15:59 +0900

    fix: Footer 레이아웃 배치 수정

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,26 +3,25 @@ import styled from "styled-components";
 
 const Main = styled.main`
   display: flex;
-  // justify-content: space-between;
   width: 100%;
   flex-direction: column;
+  position: fixed;
+  margin-left: 100px;
+  bottom: 0;
 `;
 
 // 배경
 const Footerdiv = styled.div`
-  
   background-color: #604770;
   height: 100px;
   display: flex;
-  //align-items: center;
   flex-direction: column;
   justify-content: center;
-  margin-left: 6rem;
-  //position: fixed;
-`
+  text-align: left;
+`;
 // 왼쪽 글씨
 const FooterInfo = styled.p`
-  width: 420px; 
+  width: 420px;
   font-size: 20px;
   color: white;
   // margin: 25px;
@@ -30,7 +29,7 @@ const FooterInfo = styled.p`
   //text-align: center;
   //height: 50px;
   margin-left: 5rem;
-`
+`;
 // 오른쪽 로고
 // const FooterLogo = styled.img`
 //   float: right;
@@ -41,14 +40,13 @@ const FooterInfo = styled.p`
 const Footer = () => {
   return (
     <Main>
-
-        <Footerdiv>
-          <FooterInfo>Main Project CVS Review WEB Service 
-          Copyright 2023 LIFO.</FooterInfo>
-        </Footerdiv>
-
+      <Footerdiv>
+        <FooterInfo>
+          Main Project CVS Review WEB Service Copyright 2023 LIFO.
+        </FooterInfo>
+      </Footerdiv>
     </Main>
-  )
+  );
 };
 
 export default Footer;
