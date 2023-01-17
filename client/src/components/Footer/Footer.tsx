d5feb9b48d279aea554bf696ba3c87a8c14946a1
gkswldus <gkswldus1108@naver.com>
2023-01-17 14:11:40 +0900

    fix: Footer 레이아웃 배치 수정

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,46 +3,32 @@ import styled from "styled-components";
 
 const Main = styled.main`
   display: flex;
-  width: 100%;
-  flex-direction: column;
-  position: fixed;
   margin-left: 100px;
-  bottom: 0;
 `;
-
 // 배경
 const Footerdiv = styled.div`
-  background-color: #604770;
-  height: 100px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   text-align: left;
+  width: 100%;
+  height: 100px;
+  background-color: #604770;
 `;
 // 왼쪽 글씨
 const FooterInfo = styled.p`
-  width: 420px;
-  font-size: 20px;
+  margin: 5rem;
   color: white;
-  // margin: 25px;
-  //background-color: skyblue;
-  //text-align: center;
-  //height: 50px;
-  margin-left: 5rem;
+  font-size: 20px;
 `;
-// 오른쪽 로고
-// const FooterLogo = styled.img`
-//   float: right;
-//   text-align: center;
-
-// `
 
 const Footer = () => {
   return (
     <Main>
       <Footerdiv>
         <FooterInfo>
-          Main Project CVS Review WEB Service Copyright 2023 LIFO.
+          Main Project CVS Review WEB Service Copyright <br />
+          2023 LIFO.
         </FooterInfo>
       </Footerdiv>
     </Main>
@@ -50,5 +36,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-// 모든 페이지에 연결하는거 찾아보기
