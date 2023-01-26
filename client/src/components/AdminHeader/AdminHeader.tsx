2da16bc7463c2b034d38f7c99311cc650177cbcc
jjangyeunii <euneun0927@naver.com>
2023-01-26 13:23:03 +0900

    feat: 관리자 페이지 이동 버튼 추가 및 기능 구현

--- client/src/components/AdminHeader/AdminHeader.tsx
+++ client/src/components/AdminHeader/AdminHeader.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import { LoginState } from "../../states/LoginState";
 
 const HomePageHeader = styled.header`
   display: flex;
@@ -17,16 +19,43 @@ const HomePageHeader = styled.header`
       transition: 0.5s;
     }
   }
+  button {
+    font-family: "Do Hyeon", sans-serif;
+    position: fixed;
+    right: 50px;
+    border: none;
+    border-radius: 10px;
+    background-color: #7a7979;
+    padding: 0.5rem 1rem;
+    color: #fff;
+    top: 20px;
+    &:hover {
+      cursor: pointer;
+      box-shadow: 1px 1px 2px #464646;
+    }
+  }
 `;
 
 const HomeHeader = () => {
   const navigate = useNavigate();
+  const isLogin = useRecoilValue(LoginState);
+
   const handleClickHeader = () => {
     navigate("/");
+  };
+  const handleAdminClick = () => {
+    if (!isLogin) {
+      navigate("/login");
+    } else if (isLogin && localStorage.getItem("role") === "ADMIN") {
+      navigate("/admin/search");
+    } else {
+      alert("접근 권한이 없습니다");
+    }
   };
   return (
     <HomePageHeader>
       <img src="/img/cvs logo.png" alt="logo" onClick={handleClickHeader} />
+      <button onClick={handleAdminClick}>관리자</button>
     </HomePageHeader>
   );
 };
