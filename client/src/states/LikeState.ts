3c2d01174479b5bc55c302bff21611e3ee74cc44
jjangyeunii <euneun0927@naver.com>
2023-01-31 10:39:13 +0900

    fix: 메인페이지 찜 등록 기능 수정

--- client/src/states/LikeState.ts
+++ client/src/states/LikeState.ts
@@ -1,7 +1,11 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const LikeState = (id: number, memberId: string | null) =>
   atom<boolean>({
     key: `LikeState-${memberId}-${id}`,
     default: false,
+    effects_UNSTABLE: [persistAtom],
   });
